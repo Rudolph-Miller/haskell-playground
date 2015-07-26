f05a66f39d68e07a11162bbb6a9bec34174c6631
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-07-26 23:00:03 +0900

    curry.

--- test.hs
+++ test.hs
@@ -3,3 +3,12 @@ quadruple x = double (double x)
 
 factorical n = product [1..n]
 average ns = sum ns `div` length ns
+
+add :: (Int, Int) -> Int
+add (x, y) = x + y
+
+zeroto :: Int -> [Int]
+zeroto n = [0..n]
+
+add' :: Int -> (Int -> Int)
+add' x y = x + y
